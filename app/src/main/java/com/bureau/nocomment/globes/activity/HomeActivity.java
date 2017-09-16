b8735428728b4543a7f8e9dba94a2fc235d5bef7
Hervé Bérenger <hberenger@Mac-mini-de-Herve-4.local>
2017-09-16 18:07:47 +0200

    Add language switch menu to home activity

--- app/src/main/java/com/bureau/nocomment/globes/activity/HomeActivity.java
+++ app/src/main/java/com/bureau/nocomment/globes/activity/HomeActivity.java
@@ -8,6 +8,8 @@ import android.support.v4.view.ViewCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.bureau.nocomment.globes.R;
 import com.bureau.nocomment.globes.fragment.ArchitectsFragment;
@@ -46,6 +48,24 @@ public class HomeActivity extends AppCompatActivity {
         ViewCompat.setElevation(mTabs, elevation);
         // apply elevation to homeToolbar too, otherwise the tabbar hides it
         ViewCompat.setElevation(toolbar, elevation);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.switch_language) {
+            // TODO : change locale
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     private static class HomePagerAdapter extends FragmentStatePagerAdapter {
