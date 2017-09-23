182ea4f1d3746a6f55101c968363405438983efa
Hervé Bérenger <hberenger@mac-mini-de-herve-4.home>
2017-09-24 01:07:43 +0200

    Define the asset model repository, load it at startup

--- app/src/main/java/com/bureau/nocomment/globes/application/Globes.java
+++ app/src/main/java/com/bureau/nocomment/globes/application/Globes.java
@@ -3,6 +3,8 @@ package com.bureau.nocomment.globes.application;
 import android.app.Application;
 import android.content.Context;
 
+import com.bureau.nocomment.globes.model.ModelRepository;
+
 public class Globes extends Application {
     private static Context mAppContext;
 
@@ -10,6 +12,7 @@ public class Globes extends Application {
     public void onCreate() {
         super.onCreate();
         mAppContext = this;
+        ModelRepository.getInstance().loadItemLibrary(mAppContext);
     }
 
     public static synchronized Context getAppContext() {
