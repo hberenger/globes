d220074fc41d8988705999f976b5385a9347e9a8
Hervé Bérenger <hberenger@Mac-mini-de-Herve-4.local>
2017-09-24 12:45:49 +0200

    Add fast API to retrieve project by id

--- app/src/main/java/com/bureau/nocomment/globes/model/ItemLibrary.java
+++ app/src/main/java/com/bureau/nocomment/globes/model/ItemLibrary.java
@@ -1,10 +1,18 @@
 package com.bureau.nocomment.globes.model;
 
+import junit.framework.Assert;
+
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ItemLibrary {
+    // Json ppties
     private List<Project> projects;
     private List<Table> tables;
+
+    // Computes ppties
+    private Map<Integer, Project> sortedProjects;
 
     public List<Project> getProjects() {
         return projects;
@@ -12,5 +20,22 @@ public class ItemLibrary {
 
     public List<Table> getTables() {
         return tables;
+    }
+
+    public Project findProject(int testProjectId) {
+        if (tables == null) {
+            return null;
+        }
+        if (sortedProjects == null) {
+            sortedProjects = new HashMap<>(tables.size());
+            for(Project p : projects) {
+                sortedProjects.put(p.getId(), p);
+            }
+            // Make sure we don't have any duplicate id
+            Assert.assertEquals(projects.size(), sortedProjects.size());
+            // Make sure we don't have projects without id
+            Assert.assertEquals(null, sortedProjects.get(0));
+        }
+        return sortedProjects.get(testProjectId);
     }
 }
