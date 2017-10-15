b491af8e93785479ac00cb201ac3e3aa9c48d53b
Hervé Bérenger <herve.berenger@fabernovel.com>
2017-10-15 17:25:36 +0200

    Add architects adapter

---
+++ app/src/main/java/com/bureau/nocomment/globes/adapter/ArchitectsAdapter.java
@@ -0,0 +1,43 @@
+
+package com.bureau.nocomment.globes.adapter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import com.bureau.nocomment.globes.R;
+import com.bureau.nocomment.globes.model.Project;
+import com.bureau.nocomment.globes.view.ArchitectCell;
+
+import java.util.List;
+
+public class ArchitectsAdapter extends ArrayAdapter<Project> {
+
+    private List<Project> mProjects;
+
+    public ArchitectsAdapter(Context context) {
+        super(context, 0);
+    }
+
+    public void setProjects(List<Project> projects) {
+        this.mProjects = projects;
+    }
+
+
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, @NonNull ViewGroup parent) {
+        Project project = getItem(position);
+
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext())
+                    .inflate(R.layout.view_architects_cell_root, parent, false);
+        }
+
+        ((ArchitectCell) convertView).configure(project);
+        return convertView;
+    }
+}
