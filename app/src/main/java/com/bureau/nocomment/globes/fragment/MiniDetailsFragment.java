0e7f678d8aec2eff48333777ef08d16a96a45eeb
Hervé Bérenger <herve.berenger@fabernovel.com>
2017-10-23 18:16:06 +0200

    Bind play & pause buttons in mini details

--- app/src/main/java/com/bureau/nocomment/globes/fragment/MiniDetailsFragment.java
+++ app/src/main/java/com/bureau/nocomment/globes/fragment/MiniDetailsFragment.java
@@ -5,15 +5,28 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import com.bureau.nocomment.globes.R;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class MiniDetailsFragment extends BaseFragment {
+
+    @Bind(R.id.play_button)
+    ImageButton playButton;
+
+    @Bind(R.id.pause_button)
+    ImageButton pauseButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_minidetails, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_minidetails, container, false);
+        ButterKnife.bind(this, rootView);
+        return rootView;
     }
 
     @Override
@@ -28,4 +41,27 @@ public class MiniDetailsFragment extends BaseFragment {
         super.onDetach();
     }
 
+    private void playSoundtrack() {
+//        progressUpdateHandler.post(progressUpdater);
+//        player.start();
+        playButton.setVisibility(View.GONE);
+        pauseButton.setVisibility(View.VISIBLE);
+    }
+
+    private void pauseSoundtrack() {
+//        progressUpdateHandler.removeCallbacks(progressUpdater);
+//        player.pause();
+        playButton.setVisibility(View.VISIBLE);
+        pauseButton.setVisibility(View.GONE);
+    }
+
+    @OnClick(R.id.play_button)
+    void onPlayButton(ImageButton button) {
+        playSoundtrack();
+    }
+
+    @OnClick(R.id.pause_button)
+    void onPauseButton(ImageButton button) {
+        pauseSoundtrack();
+    }
 }
