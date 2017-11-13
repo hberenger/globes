e11897ceb6472a9650d6c9f7787f3045da90eb38
Hervé Bérenger <herve.berenger@fabernovel.com>
2017-11-13 01:30:03 +0100

    Kiosk mode : start application on boot

---
+++ app/src/main/java/com/bureau/nocomment/globes/BootReceiver.java
@@ -0,0 +1,16 @@
+package com.bureau.nocomment.globes;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import com.bureau.nocomment.globes.activity.HomeActivity;
+
+public class BootReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Intent myIntent = new Intent(context, HomeActivity.class);
+        myIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(myIntent);
+    }
+}
